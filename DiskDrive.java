c433b8e8f9f6b8cd9d3b1b7e926242b9957927d1
tchan24 <tchan0524@gmail.com>
2023-11-14 13:59:44 -0600

    new file:   DiskDrive.java

---
+++ DiskDrive.java
@@ -0,0 +1,22 @@
+// DiskDrive.java
+public class DiskDrive {
+    // Implement disk drive simulation
+}
+
+// FileSystem.java
+public class FileSystem {
+    // Implement file system operations
+}
+
+// UserInterface.java
+public class UserInterface {
+    // Implement user interaction methods
+}
+
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Parse command-line arguments
+        // Initialize and start the simulation
+    }
+}

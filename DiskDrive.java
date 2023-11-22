4223d75203c1b7722eb87423c4dec71a244eb567
tchan24 <tchan0524@gmail.com>
2023-11-22 17:14:06 -0500

    added methods

--- DiskDrive.java
+++ DiskDrive.java
@@ -1,22 +1,50 @@
 public class DiskDrive {
-    private final int BLOCK_SIZE = 512;
-    private final int NUM_BLOCKS = 256;
+    public final static int BLOCK_SIZE = 512;
+    private final static int NUM_BLOCKS = 256;
     private byte[][] blocks;
+
+    public static int getBlockSize() {
+        return BLOCK_SIZE;
+    }
 
     public DiskDrive() {
         blocks = new byte[NUM_BLOCKS][BLOCK_SIZE];
-        // Initialize blocks here (e.g., setting up the FAT and bitmap)
+        // Initialize the FAT and bitmap.
+        initializeFAT();
+        initializeBitmap();
+    }
+
+    private void initializeFAT() {
+        // Initialize the first block with FAT data
+        // For simplicity, we can initially set it all to zero.
+        for(int i = 0; i < BLOCK_SIZE; i++) {
+            blocks[0][i] = 0;
+        }
+    }
+
+    private void initializeBitmap() {
+        // Initialize the second block with bitmap data.
+        // Initially, all blocks are free, except for the first two.
+        blocks[1][0] = (byte) 0b11000000; // First two blocks are used.
+        for(int i = 1; i < BLOCK_SIZE; i++) {
+            blocks[1][i] = 0;
+        }
     }
 
     public byte[] readBlock(int blockNumber) {
-        // Implement reading a block
+        if(blockNumber < 0 || blockNumber >= NUM_BLOCKS) {
+            throw new IllegalArgumentException("Invalid block number");
+        }
         return blocks[blockNumber];
     }
 
     public void writeBlock(int blockNumber, byte[] data) {
-        // Implement writing to a block
+        if(blockNumber < 0 || blockNumber >= NUM_BLOCKS) {
+            throw new IllegalArgumentException("Invalid block number");
+        }
+        if(data.length != BLOCK_SIZE) {
+            throw new IllegalArgumentException("Data size does not match block size");
+        }
         blocks[blockNumber] = data;
     }
-
-    // Additional methods for handling bitmap and FAT
 }
--- FileSystem.java
+++ FileSystem.java
@@ -1,10 +1,114 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileSystem {
     private DiskDrive diskDrive;
+    private Map<String, FileMetadata> fileTable;
+    private final static int FAT_BLOCK_NUM = 0;
+    private final static int BITMAP_BLOCK_NUM = 1;
+    private final static int MAX_FILE_SIZE = 10; // Maximum blocks per file
 
     public FileSystem(DiskDrive diskDrive) {
         this.diskDrive = diskDrive;
-        // Initialize file system structures
+        this.fileTable = new HashMap<>();
     }
 
-    // Methods for file operations (create, read, write, delete)
+    public void createFile(String fileName, byte[] data) {
+        int[] freeBlocks = findFreeBlocks(data.length);
+        if (freeBlocks.length == 0) {
+            throw new IllegalStateException("No free blocks available");
+        }
+        // Write data to the found blocks
+        for (int i = 0; i < freeBlocks.length; i++) {
+            diskDrive.writeBlock(freeBlocks[i], Arrays.copyOfRange(data, i * DiskDrive.BLOCK_SIZE, (i + 1) * DiskDrive.BLOCK_SIZE));
+        }
+        updateFAT(freeBlocks[0], freeBlocks);
+        updateBitmap(freeBlocks, true);
+    }
+    
+
+    private int[] findFreeBlocks(int dataSize) {
+        // Example implementation for contiguous allocation
+        int requiredBlocks = (int) Math.ceil((double) dataSize / DiskDrive.BLOCK_SIZE);
+        byte[] bitmap = diskDrive.readBlock(BITMAP_BLOCK_NUM);
+    
+        for (int i = 0; i < bitmap.length; i++) {
+            // Find a contiguous sequence of free blocks
+            // Implement logic based on the allocation strategy
+        }
+    
+        return new int[0]; // Placeholder for found block numbers
+    }
+    
+    public class FileMetadata {
+        private int startBlock;
+        private int length;
+    
+        public FileMetadata(int startBlock, int length) {
+            this.startBlock = startBlock;
+            this.length = length;
+        }
+    
+        public int getStartBlock() {
+            return startBlock;
+        }
+    
+        public int getLength() {
+            return length;
+        }
+    
+        // Optionally, can add setters if needed
+    }
+        
+
+    // Methods to read, update, and delete files
+
+    private void updateFAT(int fileStartBlock, int[] fileBlocks) {
+        // Update FAT with file block information
+        byte[] fatBlock = diskDrive.readBlock(FAT_BLOCK_NUM);
+        // Logic to update FAT entries
+        diskDrive.writeBlock(FAT_BLOCK_NUM, fatBlock);
+    }
+    
+    private void updateBitmap(int[] usedBlocks, boolean used) {
+        // Update bitmap to reflect block usage
+        byte[] bitmapBlock = diskDrive.readBlock(BITMAP_BLOCK_NUM);
+        // Logic to update bitmap entries
+        diskDrive.writeBlock(BITMAP_BLOCK_NUM, bitmapBlock);
+    }
+    
+    public byte[] readFile(String fileName) {
+        FileMetadata metadata = fileTable.get(fileName);
+        if (metadata == null) {
+            throw new IllegalArgumentException("File not found");
+        }
+
+        byte[] data = new byte[metadata.getLength() * DiskDrive.getBlockSize()];
+        for (int i = 0; i < metadata.getLength(); i++) {
+            System.arraycopy(diskDrive.readBlock(metadata.getStartBlock() + i), 0, data, i * DiskDrive.getBlockSize(), DiskDrive.getBlockSize());
+        }
+        return data;
+    }
+    
+    
+    public void updateFile(String fileName, byte[] newData) {
+        deleteFile(fileName);
+        createFile(fileName, newData);
+    }
+    
+    
+    public void deleteFile(String fileName) {
+        FileMetadata metadata = fileTable.get(fileName);
+        if (metadata == null) {
+            throw new IllegalArgumentException("File not found");
+        }
+
+        // Clear the FAT entries and update the bitmap
+        updateFAT(0, new int[]{metadata.getStartBlock()}); // Simplified for illustration
+        updateBitmap(new int[]{metadata.getStartBlock()}, false);
+        fileTable.remove(fileName);
+    }
+    
+    
 }
